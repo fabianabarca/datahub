7e0a11736cad50f2db8e78e6f4036f328656087b
Fabián Abarca <ensinergia@gmail.com>
2024-02-02 13:30:10 -0300

    Creación de nueva app para manejo de GTFS Realtime

--- gtfs_screens/settings.py
+++ gtfs_screens/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 INSTALLED_APPS = [
     "website.apps.WebsiteConfig",
     "gtfs.apps.GtfsConfig",
+    "realtime.apps.RealtimeConfig",
     "screens.apps.ScreensConfig",
     "django.contrib.admin",
     "django.contrib.auth",
