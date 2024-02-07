3b3a05a2d8dfd64a88541533a270ae936d184de7
Fabián Abarca <ensinergia@gmail.com>
2024-02-07 09:12:18 -0300

    Cambiar el nombre del proyecto a gtfs2screens por consistencia interna y con otros proyectos

--- gtfs2screens/wsgi.py
+++ gtfs2screens/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gtfs_screens project.
+WSGI config for gtfs2screens project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gtfs_screens.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gtfs2screens.settings")
 
 application = get_wsgi_application()
