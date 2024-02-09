2de70df25169dee66e6483ec377afa0be6b50ecd
Fabián Abarca <ensinergia@gmail.com>
2024-02-09 11:00:03 -0300

    Crear pruebas de Celery y Celery Beat con API y acceso a base de datos

--- realtime/views.py
+++ realtime/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from .tasks import test_celery, hello_celery
 
 # Create your views here.
+
+
+def test(request):
+    joke = test_celery.delay()
+    return HttpResponse(joke.get())
+
+
+def hello(request):
+    hello_celery.delay(2, 3)
+    return HttpResponse("Hello, world!")
