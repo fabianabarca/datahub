ce415e8a744028a44dc10f80eb6154393c6aae4e
Fabián Abarca <ensinergia@gmail.com>
2024-02-12 14:44:55 -0300

    Esbozo (no funcional) de configuración para conectar Celery con WebSockets

--- realtime/tasks.py
+++ realtime/tasks.py
@@ -4,6 +4,9 @@ from .models import Test
 
 from celery import shared_task
 
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 import requests
 from time import sleep
 
@@ -13,6 +16,14 @@ def test_celery():
     response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
     text = response.json()["text"]
     Test.objects.create(text=text)
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        "screen",
+        {
+            "type": "screen_message",
+            "message": text,
+        },
+    )
     return text
 
 
--- screens/routing.py
+++ screens/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from .consumers import ChatConsumer
+from .consumers import ChatConsumer, ScreenConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/screen/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
+    re_path(r"ws/screen/(?P<screen_id>\w+)/$", ScreenConsumer.as_asgi()),
 ]
