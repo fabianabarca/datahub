e4f87f1005fac9289ba4f3a197d89d8bff167dde
Fabián Abarca <ensinergia@gmail.com>
2024-02-13 14:07:21 -0300

    Crear modelo Stop auxiliar solamente para que funcione el código de prueba en views.py

--- gtfs/models.py
+++ gtfs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stop(models.Model):
+    stop_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
