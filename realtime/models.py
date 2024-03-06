fb595e8a8a4dac8d49327211cd22e348527f1286
Fabián Abarca <ensinergia@gmail.com>
2024-03-06 12:44:13 -0300

    Revisar las migraciones y corregir la falta de id en algunos modelos (con una confusión por haber puesto un string de valor por defecto cuando era un bigint)

--- realtime/models.py
+++ realtime/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Test(models.Model):
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.created_at}: {self.text}"
--- website/models.py
+++ website/models.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 
 
 class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company = models.CharField(max_length=100)
-    position = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
+    position = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
