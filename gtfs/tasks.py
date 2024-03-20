839a8d7284f082a1961cfc459357899b0f0a4684
Fabián Abarca <ensinergia@gmail.com>
2024-03-19 22:23:01 -0300

    Primera pruebas de importación de tablas a la base de datos (parcialmente exitosas)

--- gtfs/tasks.py
+++ gtfs/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
 
 import logging
-import configparser
-import datetime
-from sqlalchemy import create_engine
+from datetime import datetime
+import pytz
+import zipfile
+import io
 import pandas as pd
 import requests
 from asgiref.sync import async_to_sync, sync_to_async
@@ -13,86 +14,86 @@ from .models import *
 
 @shared_task
 def get_schedule():
-   
+
     # Logging configuration
     logging.basicConfig(
-        filename="schedule.log",
         format="%(levelname)s: %(message)s",
         encoding="utf-8",
         level=logging.INFO,
     )
 
-    # Configuration file
-    # config = configparser.ConfigParser()
-    # config.read("./gtfs.cfg")
-
-    # Database information (get from .env file? It'd be better)
-    system = "postgresql" # config.get("database", "system")
-    host = "localhost" # config.get("database", "host")
-    port = 5432 # config.get("database", "port")
-    name = "gtfs2screens" # config.get("database", "name")
-    user = "fabian" # config.get("database", "user")
-    password = "" # config.get("database", "password")
-
-    # Create database engine
-    engine = create_engine(f"{system}://{user}:{password}@{host}:{port}/{name}")
-
     # GTFS information
-    feed_transit_system = "MBTA" # config.get("gtfs", "transit_system")
-    schedule_url = "https://cdn.mbta.com/MBTA_GTFS.zip" # config.get("gtfs", "schedule_url")
-    tables = {
-        "agency": "Agency",
-        "stops": "Stop",
-        "shapes": "Shape",
-        "calendar": "Calendar",
-        "calendar_dates": "CalendarDate",
-        "routes": "Route",
-        "trips": "Trip",
-        "stop_times": "StopTime",
-        "frequencies": "Frequency",
-        "feed_info": "FeedInfo",
-    }  # They must be loaded in this order
+    company = "MBTA"
+    schedule_url = "https://cdn.mbta.com/MBTA_GTFS.zip"
 
     logging.info(
-        f"New GTFS Schedule updating session\n{feed_transit_system}\n{datetime.datetime.now()}\nData source: {schedule_url}"
+        f"GTFS Schedule updating session\n{company}\n{datetime.now()}\nData source: {schedule_url}"
     )
 
     # Check if the feed has been updated
     last_feed_tag = {"value": None}
     try:
         last_feed_tag["value"] = (
-            Feed.objects.all()
-            .order_by("-retrieved_at")
-            .first()
-            .http_etag
+            Feed.objects.all().order_by("-retrieved_at").first().http_etag
         )
     except:
-        logging.info("No http_etag found in the table 'feeds'!")
-        logging.info("A new feed will be imported!")
+        logging.info("No records found in the table 'feeds'.")
 
-    logging.info(f"Performing fetch at {datetime.datetime.now()}")
-
+    # Get the feed's ETag to compare with the last one
     schedule_check = requests.head(schedule_url)
     feed_tag = schedule_check.headers["ETag"]
 
     if not feed_tag == last_feed_tag["value"]:
-        logging.info(f"New GTFS Schedule feed detected: {feed_tag}")
-        logging.info("Importing new feed")
+        logging.info(f"Importing new GTFS Schedule feed detected: {feed_tag}")
 
-        # Save new Feed record
-        last_modified = datetime.datetime.strptime(
-            schedule_check.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z"
-        )
-        feed_id = last_modified.strftime("%Y-%m-%dT%H:%M:%S")
-        # Save to database with the Feed model
-        Feed.objects.create(
+        # Request feed
+        schedule_response = requests.get(schedule_url)
+        schedule_zip = zipfile.ZipFile(io.BytesIO(schedule_response.content))
+
+        last_modified = schedule_check.headers["Last-Modified"]
+        last_modified = datetime.strptime(last_modified, "%a, %d %b %Y %H:%M:%S %Z")
+        last_modified = last_modified.replace(tzinfo=pytz.UTC)
+        feed_id = f"{company}-{int(last_modified.timestamp())}"
+
+        # Save feed record to database with the Feed model
+        feed = Feed.objects.create(
             feed_id=feed_id,
             http_etag=feed_tag,
-            last_modified=last_modified,
+            http_last_modified=last_modified,
         )
 
+        tables = {
+            "agency": "Agency",
+            "stops": "Stop",
+            "shapes": "Shape",
+            "calendar": "Calendar",
+            "calendar_dates": "CalendarDate",
+            "routes": "Route",
+            "trips": "Trip",
+            "stop_times": "StopTime",
+            "feed_info": "FeedInfo",
+        }  # They must be loaded in this order
+
+        # Import and save tables
+        for table_name in tables.keys():
+            file = f"{table_name}.txt"
+            if file in schedule_zip.namelist():
+                model = eval(f"{tables[table_name]}")
+                fields = [field.name for field in model._meta.fields]
+                table = pd.read_csv(
+                    schedule_zip.open(file),
+                    dtype=str,
+                    keep_default_na=False,
+                    na_values="",
+                )
+                table = table[[col for col in fields if col in table.columns]]
+                table["feed"] = feed
+                objects = [model(**row) for row in table.to_dict(orient="records")]
+                model.objects.bulk_create(objects)
+                logging.info(f"{file} imported successfully")
+
     Record.objects.create(
-        data_source = "https://cdn.mbta.com/MBTA_GTFS.zip",
+        data_source="https://cdn.mbta.com/MBTA_GTFS.zip",
     )
 
     return "Fetching Schedule"
