208ae594a91dbd9e936a2c7f6021e30cc4dc6a88
Fabián Abarca <ensinergia@gmail.com>
2024-04-20 14:44:34 -0300

    Cambiar app screens a alerts para un futuro sistema de alertas complementario al API

--- datahub/asgi.py
+++ datahub/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from screens.routing import websocket_urlpatterns
+from alerts.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datahub.settings")
 
