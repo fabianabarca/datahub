b62de32e1132c340a7f6e5b96c4e74d2689fbce3
Fabián Abarca <ensinergia@gmail.com>
2024-04-25 10:52:41 -0300

    Configurar API con autorización por token

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.http import FileResponse
+from feed.models import Application
+from rest_framework import viewsets, permissions, authentication
 
-# Create your views here.
+from .serializers import ApplicationSerializer
+
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    """
+    Aplicaciones conectadas al servidor de datos.
+    """
+
+    queryset = Application.objects.all().order_by("created_at")
+    serializer_class = ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+
+def get_schema(request):
+    file_path = settings.BASE_DIR / "api" / "datahub.yml"
+    return FileResponse(
+        open(file_path, "rb"), as_attachment=True, filename="datahub.yml"
+    )
