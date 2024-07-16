0f9686afb6343a0b68c857596401186c66328e80
Fabián Abarca <ensinergia@gmail.com>
2024-07-16 12:58:29 -0300

    Agregar GISModelAdmin para tablas con datos geoespaciales

--- gtfs/admin.py
+++ gtfs/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from .models import *
 
@@ -7,12 +7,12 @@ from .models import *
 admin.site.register(GTFSProvider)
 admin.site.register(Feed)
 admin.site.register(Agency)
-admin.site.register(Stop)
+admin.site.register(Stop, admin.GISModelAdmin)
 admin.site.register(Route)
 admin.site.register(Calendar)
 admin.site.register(CalendarDate)
 admin.site.register(Shape)
-admin.site.register(GeoShape)
+admin.site.register(GeoShape, admin.GISModelAdmin)
 admin.site.register(Trip)
 admin.site.register(StopTime)
 admin.site.register(FeedInfo)
@@ -21,4 +21,4 @@ admin.site.register(FareRule)
 admin.site.register(FeedMessage)
 admin.site.register(TripUpdate)
 admin.site.register(StopTimeUpdate)
-admin.site.register(VehiclePosition)
+admin.site.register(VehiclePosition, admin.GISModelAdmin)
