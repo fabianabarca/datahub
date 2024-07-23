544c0be4bbc4e811ed81a4720a868ec4b6fd2c90
David Segura <david_d_sc@hotmail.com>
2024-07-23 01:59:08 -0600

    Inclusión de shapes.json completo y creación de algunos registers agencies, calendar. calen..dates, correción de nombres en json

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,10 @@ router.register(r"routes", views.RouteViewSet)
 router.register(r"trips", views.TripViewSet)
 router.register(r"stops", views.StopViewSet)
 router.register(r"shapes", views.ShapeViewSet)
+router.register(r"calendars", views.CalendarViewSet)
+router.register(r"calendar-dates", views.CalendarDateViewSet)
+router.register(r"agencies", views.AgencyViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
