c769a6e1c84003711f139d0167b679eeb58540b0
Josue Vargas <jossuevargasces@gmail.com>
2024-07-28 09:01:59 -0600

    create_fixtures.py funcionando para hacer loaddata

--- gtfs/fixtures/gtfs/create_fixtures.py
+++ gtfs/fixtures/gtfs/create_fixtures.py
@@ -14,7 +14,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 django.setup()
 
 from datetime import datetime
-from django.db.models import DateField
+from django.db.models import DateField, IntegerField, FloatField, DecimalField
+from django.contrib.gis.geos import Point
 from django.apps import apps
 from gtfs.models import *
 
@@ -47,6 +48,8 @@ tab_to_model_mapping = {
     "routes": ("gtfs.route", model_field_mapping["route"]),
     "calendar": ("gtfs.calendar", model_field_mapping["calendar"]),
     "calendar_dates": ("gtfs.calendardate", model_field_mapping["calendardate"]),
+    "stops": ("gtfs.stop", model_field_mapping["stop"]),
+    "stop_times": ("gtfs.stoptime", model_field_mapping["stoptime"]),
 }
 
 
@@ -55,13 +58,13 @@ xls = pd.ExcelFile(excel_file_path)
 
 # Maximum number of rows to process from each sheet
 # Set to None to process all rows (used for debugging)
-max_rows_per_sheet = ""
+max_rows_per_sheet = 2
 
 # Initialize an empty list for fixtures
 fixtures = []
 
 # Initialize a counter for primary keys
-pk_counter = 1
+pk_counter = 2  # Start at 2 to avoid conflicts with the existing fixtures
 
 # Process each sheet in the Excel file
 for sheet_name in xls.sheet_names:
@@ -84,26 +87,110 @@ for sheet_name in xls.sheet_names:
             for field in model_fields:
                 # Set the field type
                 field_type = model_class._meta.get_field(field)
+                field_value = row.get(field, None)
+
+                # Convert NaN to None
+                if pd.isna(field_value) or field_value == '':
+                    field_value = None
+
                 if isinstance(field_type, DateField):
                     # Convert the string to a date object
-                    field_value = row.get(field, None)
                     field_value = (
                         datetime.strptime(str(field_value), "%Y%m%d").date().isoformat()
-                        if field_value
+                        if field_value or field_value != ""
                         else None
                     )
                     fields_data[field] = field_value
+                elif field_type.get_internal_type() == "PointField":
+                    # Convert the string to a Point object
+                    if field_value or field_value != "":
+                        #point_str = field_value.split('POINT (')[1].strip(')')
+                        #longitude, latitude = map(float, point_str.split())
+                        #fields_data[field] = (longitude,latitude)
+                        fields_data[field] = field_value
+                    else:
+                        fields_data[field] = None
+                elif field_type.get_internal_type() == "IntegerField":
+                    # Convert the string to an integer
+                    fields_data[field] = int(field_value) if field_value is not None else None
+                elif field_type.get_internal_type() == "FloatField":
+                    # Convert the string to a float
+                    fields_data[field] = float(field_value) if field_value else None
+                elif field_type.get_internal_type() == "CharField":
+                    # Convert the string to a char
+                    fields_data[field] = str(field_value) if field_value else None
+                elif field_type.get_internal_type() == "TextField":
+                    # Convert the string to a text
+                    fields_data[field] = str(field_value) if field_value else None
+                elif field_type.get_internal_type() == "TimeField":
+                    # Convert the string to a time object
+                    fields_data[field] = (
+                        datetime.strptime(str(field_value), "%H:%M:%S").time().isoformat()
+                        if field_value
+                        else None
+                    )
+                #elif field_type.get_internal_type() == "ForeignKey":
+                    # Get the related model name
+                #    related_model_name = field_type.related_model._meta.model_name
+                    # Get the related model's primary key
+                #    related_pk = row.get(f"{field}_id", None)
+                    # Set the field value to the related model's primary key
+                #    fields_data[field] = related_pk
                 else:
-                    # Set the field value to the row's value if it exists, else null
-                    fields_data[field] = row.get(field, None)
+                    # Set the field value to the row's value
+                    fields_data[field] = field_value
 
             # Add a fixed field 'feed' with a value of "1234" for 'gtfs.stop' and 'gtfs.route'
             if "feed" in model_fields:
-                fields_data["feed"] = "1234"
+                fields_data["feed"] = "1"
+
+            #if "parent_station" in model_fields and fields_data.get("parent_station") is None:
+            #    fields_data["parent_station"] = "Estacion Principal"  # or some default value
+            #if "stop_timezone" in model_fields and fields_data.get("stop_timezone") is None:
+            #    fields_data["stop_timezone"] = ""  # or some default value
+
+            # Ensure all other fields are not null
+            for key in fields_data:
+                if fields_data[key] is None and not isinstance(model_class._meta.get_field(key), IntegerField) and not isinstance(model_class._meta.get_field(key), FloatField) and not isinstance(model_class._meta.get_field(key), DecimalField):
+                    fields_data[key] = ""
 
             fixture = {"model": model_name, "pk": pk_counter, "fields": fields_data}
             fixtures.append(fixture)
             pk_counter += 1  # Increment the pk counter for the next fixture
+
+# Additional data to be added. These are not part of the Excel file but needed for other models to work
+additional_data = [
+    {
+        "model": "gtfs.gtfsprovider",
+        "pk": 1,
+        "fields": {
+            "code": "bUCR",
+            "name": "bUCR",
+            "description": "Bus de la UCR",
+            "website": "https://bucr.digital",
+            "schedule_url": None,
+            "trip_updates_url": None,
+            "vehicle_positions_url": None,
+            "service_alerts_url": None,
+            "timezone": "America/costa_rica",
+            "is_active": True,
+        },
+    },
+    {
+        "model": "gtfs.feed",
+        "pk": 1,
+        "fields": {
+            "provider": 1,
+            "http_etag": None,
+            "http_last_modified": "2024-07-11T00:00:00Z",
+            "is_current": True,
+            "retrieved_at": "2024-07-11T04:28:41.332Z",
+        },
+    },
+]
+
+# Append the additional data to the fixtures list
+fixtures.extend(additional_data)
 
 # Write fixtures to a file
 with open("./gtfs/fixtures/gtfs.json", "w") as f:
