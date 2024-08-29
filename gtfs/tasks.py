7361abb0d10ad22807c12863f7876b2bdb3f2b60
Fabián Abarca <ensinergia@gmail.com>
2024-08-29 20:49:14 -0300

    Inclusión y revisión de get_tripupdates para completar importación de GTFS Realtime (sin alertas)

--- gtfs/tasks.py
+++ gtfs/tasks.py
@@ -109,6 +109,7 @@ def get_vehiclepositions():
         vehicle_positions_response = requests.get(provider.vehicle_positions_url)
         vehicle_positions.ParseFromString(vehicle_positions_response.content)
 
+        # Save feed message to database
         feed_message = FeedMessage(
             feed_message_id=f"{provider.code}-vehicle-{vehicle_positions.header.timestamp}",
             provider=provider,
@@ -178,7 +179,139 @@ def get_vehiclepositions():
 
 @shared_task
 def get_tripupdates():
-    return "Fetching TripUpdates"
+    providers = GTFSProvider.objects.filter(is_active=True)
+    for provider in providers:
+        try:
+            trip_updates_response = requests.get(provider.trip_updates_url, timeout=10)
+            trip_updates_response.raise_for_status()
+        except requests.RequestException as e:
+            print(
+                f"Error fetching trip updates from {provider.trip_updates_url}: {str(e)}"
+            )
+            continue
+
+        # Parse FeedMessage object from Protobuf
+        trip_updates = gtfs_rt.FeedMessage()
+        trip_updates.ParseFromString(trip_updates_response.content)
+
+        # Build FeedMessage object
+        feed_message = FeedMessage(
+            feed_message_id=f"{provider.code}-trip_updates-{trip_updates.header.timestamp}",
+            provider=provider,
+            entity_type="trip_update",
+            timestamp=datetime.fromtimestamp(
+                int(trip_updates.header.timestamp),
+                tz=pytz.timezone(provider.timezone),
+            ),
+            incrementality=trip_updates.header.incrementality,
+            gtfs_realtime_version=trip_updates.header.gtfs_realtime_version,
+        )
+        # Save FeedMessage object
+        feed_message.save()
+
+        # Build TripUpdate DataFrame
+        trip_updates_json = json_format.MessageToJson(
+            trip_updates, preserving_proto_field_name=True
+        )
+        trip_updates_json = json.loads(trip_updates_json)
+        trip_updates_df = pd.json_normalize(trip_updates_json["entity"], sep="_")
+        trip_updates_df.rename(columns={"id": "entity_id"}, inplace=True)
+        trip_updates_df["feed_message"] = feed_message
+
+        # Fix entity timestamp
+        trip_updates_df["trip_update_timestamp"].fillna(
+            datetime.now().timestamp(), inplace=True
+        )
+        trip_updates_df["trip_update_timestamp"] = pd.to_datetime(
+            trip_updates_df["trip_update_timestamp"].astype(int), unit="s", utc=True
+        )
+        # Fix trip start date
+        trip_updates_df["trip_update_trip_start_date"] = pd.to_datetime(
+            trip_updates_df["trip_update_trip_start_date"], format="%Y%m%d"
+        )
+        trip_updates_df["trip_update_trip_start_date"].fillna(
+            datetime.now().date(), inplace=True
+        )
+        # Fix trip start time
+        trip_updates_df["trip_update_trip_start_time"] = pd.to_timedelta(
+            trip_updates_df["trip_update_trip_start_time"]
+        )
+        trip_updates_df["trip_update_trip_start_time"].fillna(
+            timedelta(hours=0, minutes=0, seconds=0), inplace=True
+        )
+        # Fix trip direction
+        trip_updates_df["trip_update_trip_direction_id"].fillna(-1, inplace=True)
+        
+
+        for i, trip_update in trip_updates_df.iterrows():
+            this_trip_update = TripUpdate(
+                entity_id=trip_update["entity_id"],
+                feed_message=trip_update["feed_message"],
+                trip_trip_id=trip_update["trip_update_trip_trip_id"],
+                trip_route_id=trip_update["trip_update_trip_route_id"],
+                trip_direction_id=trip_update["trip_update_trip_direction_id"],
+                trip_start_time=trip_update["trip_update_trip_start_time"],
+                trip_start_date=trip_update["trip_update_trip_start_date"],
+                trip_schedule_relationship=trip_update[
+                    "trip_update_trip_schedule_relationship"
+                ],
+                vehicle_id=trip_update["trip_update_vehicle_id"],
+                vehicle_label=trip_update["trip_update_vehicle_label"],
+                # trip_update_vehicle_license_plate=trip_update["trip_update_vehicle_license_plate"],
+                # trip_update_vehicle_wheelchair_accessible=trip_update["trip_update_vehicle_wheelchair_accessible"],
+                timestamp=trip_update["trip_update_timestamp"],
+                # trip_update_delay=trip_update["trip_update_delay"],
+            )
+            # Save this TripUpdate object
+            this_trip_update.save()
+
+            # Build StopTimeUpdate DataFrame
+            stop_time_updates_json = str(trip_update["trip_update_stop_time_update"])
+            stop_time_updates_json = stop_time_updates_json.replace("'", '"')
+            stop_time_updates_json = json.loads(stop_time_updates_json)
+            stop_time_updates_df = pd.json_normalize(stop_time_updates_json, sep="_")
+            stop_time_updates_df["feed_message"] = feed_message
+            stop_time_updates_df["trip_update"] = this_trip_update
+
+            # Fix arrival time timestamp
+            if "arrival_time" in stop_time_updates_df.columns:
+                stop_time_updates_df["arrival_time"].fillna(
+                    datetime.now().timestamp(), inplace=True
+                )
+                stop_time_updates_df["arrival_time"] = pd.to_datetime(
+                    stop_time_updates_df["arrival_time"].astype(int), unit="s", utc=True
+                )
+            # Fix departure time timestamp
+            if "departure_time" in stop_time_updates_df.columns:
+                stop_time_updates_df["departure_time"].fillna(
+                    datetime.now().timestamp(), inplace=True
+                )
+                stop_time_updates_df["departure_time"] = pd.to_datetime(
+                    stop_time_updates_df["departure_time"].astype(int),
+                    unit="s",
+                    utc=True,
+                )
+            # Fix arrival uncertainty
+            if "arrival_uncertainty" in stop_time_updates_df.columns:
+                stop_time_updates_df["arrival_uncertainty"].fillna(0, inplace=True)
+            # Fix departure uncertainty
+            if "departure_uncertainty" in stop_time_updates_df.columns:
+                stop_time_updates_df["departure_uncertainty"].fillna(0, inplace=True)
+            # Fix arrival delay
+            if "arrival_delay" in stop_time_updates_df.columns:
+                stop_time_updates_df["arrival_delay"].fillna(0, inplace=True)
+            # Fix departure delay
+            if "departure_delay" in stop_time_updates_df.columns:
+                stop_time_updates_df["departure_delay"].fillna(0, inplace=True)
+
+            # Save to database
+            objects = [
+                StopTimeUpdate(**row)
+                for row in stop_time_updates_df.to_dict(orient="records")
+            ]
+            StopTimeUpdate.objects.bulk_create(objects)
+
+    return "TripUpdates saved to database"
 
 
 @shared_task
